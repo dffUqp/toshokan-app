afec6ea5fe75728c61ee8666cf600b3d79df3372
dffUqp <yuriivasilchuk.work@gmail.com>
2022-11-08 23:33:16 +0200

    remove large gap between rows on GalleryPage

--- src/pages/Anime/partials/Gallery/index.tsx
+++ src/pages/Anime/partials/Gallery/index.tsx
@@ -9,13 +9,13 @@ const Gallery = (): JSX.Element => {
   }
 
   return (
-    <div className="flex flex-wrap gap-5 max-w-[1100px] w-full mx-auto md:justify-center">
+    <div className="flex flex-wrap gap-5 max-w-[1100px] w-full mx-auto md:justify-center content-start">
       {galleryData.data.map((item, index) => (
         <ImageWithPlaceholder
           key={index}
           src={item.jpg.large_image_url}
           alt="anime"
-          className="w-[200px] h-auto max-h-[315px]"
+          className="w-[200px] max-h-[315px]"
         />
       ))}
     </div>
